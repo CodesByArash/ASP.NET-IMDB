588fa4ef86a6ad187b9591a635541d13bb46e7b7
arash <arash.m.gholinejad@gmail.com>
2025-04-05 19:10:46 +0330

    updated aplication db context by adding relations

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -5,13 +5,70 @@ using System.Threading.Tasks;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace api.Data
+public class MovieDatabaseContext : DbContext
 {
-    public class ApplicationDBContext:DbContext
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Series> Series { get; set; }
+    public DbSet<Season> Seasons { get; set; }
+    public DbSet<Episode> Episodes { get; set; }
+    public DbSet<UserRating> UserRatings { get; set; }
+    public DbSet<Cast> Casts { get; set; }
+    public DbSet<Person> People { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public ApplicationDBContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        if (!optionsBuilder.IsConfigured)
         {
-            public DbSet<Movie>
+            optionsBuilder.UseSqlServer("  ");
         }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        modelBuilder.Entity<UserRating>()
+            .HasOne(ur => ur.Movie)
+            .WithMany(m => m.UserRatings)
+            .HasForeignKey(ur => ur.MovieId);
+
+        modelBuilder.Entity<UserRating>()
+            .HasOne(ur => ur.Series)
+            .WithMany(s => s.UserRatings)
+            .HasForeignKey(ur => ur.SeriesId);
+
+        modelBuilder.Entity<UserRating>()
+            .HasOne(ur => ur.Season)
+            .WithMany(s => s.UserRatings)
+            .HasForeignKey(ur => ur.SeasonId);
+
+        modelBuilder.Entity<UserRating>()
+            .HasOne(ur => ur.Episode)
+            .WithMany(e => e.UserRatings)
+            .HasForeignKey(ur => ur.EpisodeId);
+
+        modelBuilder.Entity<UserRating>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRatings)
+            .HasForeignKey(ur => ur.UserId);
+
+        modelBuilder.Entity<Cast>()
+            .HasOne(c => c.Movie)
+            .WithMany(m => m.Casts)
+            .HasForeignKey(c => c.MovieId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Cast>()
+            .HasOne(c => c.Episode)
+            .WithMany(e => e.Casts)
+            .HasForeignKey(c => c.EpisodeId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.UserRatings)
+            .WithOne(ur => ur.User)
+            .HasForeignKey(ur => ur.UserId);
     }
 }
