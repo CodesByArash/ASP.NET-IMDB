df7152357bbe36ca37201272c077871749bc3241
arash <arash.m.gholinejad@gmail.com>
2025-05-10 20:49:36 +0330

    login dto gets UserName instead of email bug fixed

--- api/Dtos/Account/LoginDto.cs
+++ api/Dtos/Account/LoginDto.cs
@@ -6,7 +6,7 @@ public class LoginDto
 {
     [Required(ErrorMessage = "Email address is required")]
     [EmailAddress(ErrorMessage = "Invalid email address format")]
-    public string EmailAddress { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = string.Empty;
